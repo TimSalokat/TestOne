6b44ae0ea38f880719e5d047cec18705e8d0dbdc
TimsLaptop <V0idOne@protonmail.com>
2021-01-20 10:43:20 +0100

    changes x2

--- TimCodesShit/ROT encoder and decoder.py
+++ TimCodesShit/ROT encoder and decoder.py
@@ -1,5 +1,5 @@
 #
-#These are comments with my TODO
+#These are comments with my TODO lüge
 #Program which takes text input and then uses all or just one ROT encryption and prints it out.
 #if in the encoded text is a word like "der die das" or smth. mark it green or red idk. which color.
 #
