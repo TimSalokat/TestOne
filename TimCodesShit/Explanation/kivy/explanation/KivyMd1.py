f072253aa480f10c33596dc9f165b08361583a08
Tim <Tim.salokat@outlook.com>
2021-02-07 20:27:00 +0100

    learned more shit

--- TimCodesShit/Explanation/kivy/explanation/KivyMd1.py
+++ TimCodesShit/Explanation/kivy/explanation/KivyMd1.py
@@ -4,6 +4,9 @@ from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.uix.screen import Screen
 from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDFloatingActionButton
 from kivy.lang import Builder
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import OneLineListItem, MDList, TwoLineListItem, ThreeLineListItem, ThreeLineIconListItem, IconLeftWidget, ThreeLineAvatarIconListItem, ImageLeftWidget #the 2 before are for custom images
+from kivy.uix.scrollview import ScrollView
 
 global center
 center = {"center_x":0.5, "center_y":0.5}
@@ -44,29 +47,107 @@ center = {"center_x":0.5, "center_y":0.5}
 #         screen.add_widget(btn_flat)
 #         return screen
 
+#---UserInput---
+# class TestApp(MDApp):
+
+#     def build(self):
+#         self.theme_cls.primary_palette = "Purple"
+#         self.theme_cls.primary_hue = "300"
+#         self.theme_cls.theme_style = "Light"
+
+#         screen = Screen()
+
+#         button = MDRectangleFlatButton(text="Submit", pos_hint={"center_x":0.5, "center_y":0.4},
+#                                         on_release=self.show_input)
+#         self.username = Builder.load_string(username_helper)
+
+#         screen.add_widget(self.username)
+#         screen.add_widget(button)
+
+#         return screen
+
+#     def show_input(self, obj):
+
+#         if self.username.text == "":
+#             check_string = "Please enter a username"
+#         else:
+#             check_string = self.username.text + " does not exist"
+
+#         close_button = MDFlatButton(text="Close", on_release=self.close_dialog)
+#         more_button = MDFlatButton(text="more")
+
+#         self.dialog = MDDialog(text=f"{check_string}", title="Username", size_hint=(0.6,1), buttons=[close_button, more_button])
+#         self.dialog.open()
+
+#         print(self.username.text)
+#         self.username.text = ""
+    
+#     def close_dialog(self,obj):
+#         self.dialog.dismiss()
+#---end user input---
+
+#hard coded lists
+# class TestApp(MDApp):
+    
+#     def build(self):
+
+#         screen = Screen()
+#         scroll_view = ScrollView()
+#         list_view = MDList()
+
+#         for _ in range(10):
+#             #image = ImageLeftWidget(source="example.png")
+#             icon = IconLeftWidget(icon="laptop")
+#             items = ThreeLineIconListItem(text=f"Item {_ + 1}", secondary_text="Description", tertiary_text="More text")
+#             list_view.add_widget(items) 
+
+#             items.add_widget(icon)#or image
+
+
+#         scroll_view.add_widget(list_view)
+#         screen.add_widget(scroll_view)
+
+#         return screen
+
+
+#string lists with ids
+# list_helper = """
+# Screen:
+#     ScrollView:
+#         MDList:
+#             id: container
+
+# """
+
+# class TestApp(MDApp):
+#     def build(self):
+#         screen = Builder.load_string(list_helper)
+
+#         return screen
+
+#     def on_start(self):
+#         for _ in range(20):
+#             items = OneLineListItem(text=f"Item {_ + 1}")
+#             self.root.ids.container.add_widget(items)
+
+
+screen_helper = """
+Screen:
+    BoxLayout:
+        orientation: "vertical"
+        MDToolbar:
+            title: "Demo"
+        MDLabel:
+            text: "Hello world!"
+            halign: "center"
+"""
 
 class TestApp(MDApp):
 
     def build(self):
-        self.theme_cls.primary_palette = "Purple"
-        self.theme_cls.primary_hue = "300"
-        self.theme_cls.theme_style = "Light"
-
-        screen = Screen()
-
-        button = MDRectangleFlatButton(text="Submit", pos_hint={"center_x":0.5, "center_y":0.4},
-                                        on_release=self.show_input)
-        self.username = Builder.load_string(username_helper)
-
-        screen.add_widget(self.username)
-        screen.add_widget(button)
+        screen = Builder.load_string(screen_helper)
 
         return screen
-
-    def show_input(self, obj):
-        print(self.username.text)
-        self.username.text = ""
-
 
 
 
