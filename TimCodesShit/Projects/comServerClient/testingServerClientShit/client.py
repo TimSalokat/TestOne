558ed2423febe9e2ae7f4bcba33ff676d580fdb1
Tim <Tim.salokat@outlook.com>
2021-02-07 14:38:56 +0100

    changed many stuff

--- TimCodesShit/Projects/comServerClient/testingServerClientShit/client.py
+++ TimCodesShit/Projects/comServerClient/testingServerClientShit/client.py
@@ -5,7 +5,11 @@ target_port = 1234
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
-message = input("Input: ")
-client.send(message.encode())
-response = client.recv(4096).decode()
-print(response)
+while True:
+    message = input("Input: ")
+    client.send(message.encode())
+    if message == "exit client":
+        break
+    else:
+        response = client.recv(4096).decode()
+        print(response)
