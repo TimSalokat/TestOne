ad5bcb570165477fe76104c4886702469bdd6ee3
Tim <Tim.salokat@outlook.com>
2021-02-09 16:14:43 +0100

    Updated FirstGuy ChatUI

--- TimCodesShit/Explanation/kivy/FirstGui.py
+++ TimCodesShit/Explanation/kivy/FirstGui.py
@@ -8,6 +8,16 @@ Window.size = (300,500)
 
 
 KV = """
+<Box@MDBoxLayout>
+    bg: 0,0,0,0
+
+    canvas:
+        Color:
+            rgba: root.bg
+        Rectangle:
+            pos: self.pos
+            size: self.size
+
 
 <ContentNavigationDrawer>:
     orientation: "vertical"
@@ -22,42 +32,57 @@ KV = """
                 text: "Chat"
                 on_press:
                     root.nav_drawer.set_state("close")
+                    root.screen_manager.transition.direction = "down"
                     root.screen_manager.current = "Chat"
                 IconLeftWidget:
                     icon: "chat"
                     on_press:
                         root.nav_drawer.set_state("close")
+                        root.screen_manager.transition.direction = "down"
                         root.screen_manager.current = "Chat"
 
             OneLineIconListItem:
                 text: "To-Do"
                 on_press:
                     root.nav_drawer.set_state("close")
+                    root.screen_manager.transition.direction = "down"
                     root.screen_manager.current = "Todo"
                 IconLeftWidget:
                     icon: "checkbox-marked-circle-outline"
+                    on_press:
+                        root.nav_drawer.set_state("close")
+                        root.screen_manager.transition.direction = "down"
+                        root.screen_manager.current = "Todo"
 
             OneLineIconListItem:
                 text: "Devices"
                 on_press:
                     root.nav_drawer.set_state("close")
+                    root.screen_manager.transition.direction = "down"
                     root.screen_manager.current = "Devices"
                 IconLeftWidget:
                     icon: "devices"
+                    on_press:
+                        root.nav_drawer.set_state("close")
+                        root.screen_manager.transition.direction = "down"
+                        root.screen_manager.current = "Devices"
             
             OneLineIconListItem:
                 text: "Tilih"
                 on_press:
                     root.nav_drawer.set_state("close")
+                    root.screen_manager.transition.direction = "down"
                     root.screen_manager.current = "Tilih"
                 IconLeftWidget:
                     icon: "console-line"
+                    on_press:
+                        root.nav_drawer.set_state("close")
+                        root.screen_manager.transition.direction = "down"
+                        root.screen_manager.current = "Tilih"
 
     MDList:
         OneLineIconListItem:
             text: "Logout"
-            IconLeftWidget:
-                icon: "logout"
         
 
 Screen:
@@ -68,7 +93,7 @@ Screen:
         Screen:
             name: "Home"
             
-            BoxLayout:
+            MDBoxLayout:
                 orientation: "vertical"
 
                 MDToolbar:
@@ -81,7 +106,7 @@ Screen:
         Screen:
             name: "Chat"
 
-            BoxLayout:
+            MDBoxLayout:
                 orientation: "vertical"
 
                 MDToolbar:
@@ -89,12 +114,34 @@ Screen:
                     elevation: 10
                     left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
 
-                Widget:
+                Box:
+                    bg: 177/255,230/255,229/255,.7
+                    ScrollView:
+                            
+                        MDLabel:
+                            id: ChatBox
+                            text: "Text"
+                Box:
+                    bg: 177/255,230/255,229/255,.7
+                    adaptive_height: True
+
+                    MDTextField:
+                        multiline: True
+                        mode: "rectangle"
+                        mode: "fill"
+                        fill_color: 177/255,230/255,229/255,.5
+                        hint_text: "Input"
+                            
+                    MDFloatingActionButton:
+                        md_bg_color: app.theme_cls.primary_color
+                        icon: "send"
+
+                       
         
         Screen:
             name: "Todo"
 
-            BoxLayout:
+            MDBoxLayout:
                 orientation: "vertical"
 
                 MDToolbar:
@@ -111,7 +158,7 @@ Screen:
         Screen:
             name: "Devices"
 
-            BoxLayout:
+            MDBoxLayout:
                 orientation: "vertical"
 
                 MDToolbar:
@@ -124,7 +171,7 @@ Screen:
         Screen:
             name: "Tilih"
             
-            BoxLayout:
+            MDBoxLayout:
                 orientation: "vertical"
 
                 MDToolbar:
@@ -138,7 +185,7 @@ Screen:
         id: nav_drawer
         orientation: "vertical"
 
-        BoxLayout:
+        MDBoxLayout:
             orientation: "horizontal"
             size_hint: None, None
 
@@ -148,7 +195,7 @@ Screen:
                 size: "100dp", "100dp"
                 source: "nyan.png"
 
-            BoxLayout:
+            MDBoxLayout:
                 orientation: "vertical"
                 size_hint: None, None
                 padding: "10dp"
@@ -175,6 +222,8 @@ Screen:
             nav_drawer: nav_drawer
             
 """
+
+
 class DrawerList():
     pass
 
@@ -185,9 +234,12 @@ class ContentNavigationDrawer(BoxLayout):
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "Purple"
-        self.theme_cls.primary_hue = "700" 
+        self.theme_cls.primary_hue = "800" 
         self.theme_cls.theme_style = "Dark"
 
         return Builder.load_string(KV)
+
+def babafunktion(self,obj):
+    print("hello")
 
 MainApp().run()
