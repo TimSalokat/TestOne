938638246c35f4e8ae05dc0c8df9c7f2645d1623
Tim <Tim.salokat@outlook.com>
2021-02-12 10:31:59 +0100

    i push it all

---
+++ TimCodesShit/Projects/comServerClient/guiClientServer/server.py
@@ -0,0 +1,101 @@
+# Import socket module
+from socket import *
+import threading
+import sys # In order to terminate the program
+
+FLAG = False  # this is a flag variable for checking quit
+
+# function for receiving message from client
+def recv_from_client(conn):
+    global FLAG
+    try:
+        # Receives the request message from the client
+        while True:
+            if FLAG == True:
+                break
+            message = conn.recv(1024).decode()
+            # if 'q' is received from the client the server quits
+            if message == 'q':
+                conn.send('q'.encode())
+                print('Closing connection')
+                conn.close()
+                FLAG = True
+                break
+            else:
+                print('\nClient: ' + message)
+    except:
+        conn.close()
+
+
+# function for receiving message from client
+def send_to_client(conn):
+    global FLAG
+    try:
+        while True:
+            if FLAG == True:
+                break
+            send_msg = input('Message: ')
+            # the server can provide 'q' as an input if it wish to quit
+            if send_msg == 'q':
+                conn.send('q'.encode())
+                print('Closing connection')
+                conn.close()
+                FLAG = True
+                break
+            conn.send(send_msg.encode())
+    except:
+        conn.close()
+
+
+# this is main function
+def server():
+    threads = []
+    global FLAG
+
+    # TODO (1) - define HOST name, this would be an IP address or 'localhost' (1 line)
+    HOST = 'localhost'
+    # TODO (2) - define PORT number (1 line) (Google, what should be a valid port number)
+    # make sure the ports are not used for any other application
+    serverPort = 6789
+
+    # Create a TCP server socket
+    #(AF_INET is used for IPv4 protocols)
+    #(SOCK_STREAM is used for TCP)
+    # TODO (3) - CREATE a socket for IPv4 TCP connection (1 line)
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    # Bind the socket to server address and server port
+    # TODO (4) - bind the socket for HOSR and serverPort (1 line)
+    serverSocket.bind((HOST, serverPort))
+
+    # Listen to at most 1 connection at a time
+    # TODO (5) - listen and wait for request from client (1 line)
+    serverSocket.listen(1)
+
+    # Server should be up and running and listening to the incoming connections
+    print('The chat server is ready to connect to a chat client')
+    # TODO (6) - accept any connection request from a client (1 line)
+    connectionSocket, addr = serverSocket.accept()
+    print('Sever is connected with a chat client\n')
+
+    t_rcv = threading.Thread(target=recv_from_client, args=(connectionSocket,))
+    t_send = threading.Thread(target=send_to_client, args=(connectionSocket,))
+    # call the function to receive message server
+    #recv_from_server(clientSocket)
+    threads.append(t_rcv)
+    threads.append(t_send)
+    t_rcv.start()
+    t_send.start()
+
+    t_rcv.join()
+    t_send.join()
+
+
+    # closing serverScoket before exiting
+    print('EXITING')
+    serverSocket.close()
+    #Terminate the program after sending the corresponding data
+    sys.exit()
+
+
+server()
--- TimCodesShit/testing aka. stealing code/importet2.py
+++ TimCodesShit/testing aka. stealing code/importet2.py
@@ -1,76 +1,61 @@
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
-
 from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel, MDIcon
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton, MDIconButton, MDFloatingActionButton
+from kivy.lang import Builder
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.list import OneLineListItem, MDList, TwoLineListItem, ThreeLineListItem, ThreeLineIconListItem, IconLeftWidget, ThreeLineAvatarIconListItem, ImageLeftWidget #the 2 before are for custom images
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
 
-KV = '''
-<ContentNavigationDrawer>:
+global center
+center = {"center_x":0.5, "center_y":0.5}
 
-    NavigationLayout:
+username_helper = """
+MDTextField:
+    hint_text: "Enter Username"
+    helper_text: "or click forgot username"
+    helper_text_mode: "on_focus"
 
-        MDNavigationDrawer:
-            NavigationDrawerToolbar:
-                title: "Helo this is a toolbar"
+    icon_right: "laptop"
+    icon_right_color: app.theme_cls.primary_color
 
+    pos_hint:{"center_x":0.5, "center_y":0.5}
+    size_hint_x:None
+    width:200
+"""#possibilities for help
 
-                NavigationDrawerIconButton:
-                    icon: "laptop"
-                    text: "Screen 1"
-                    on_release: root.screen_manager.current = "scr 1"
+button_helper = """
+MDRectangleFlatButton:
+    text: "Submit"
+    pos_hint: {"center_x":.5, "center_y":.4}
+    on_release: app.show_input()
+"""
 
-                NavigationDrawerIconButton:
-                    icon: "laptop"
-                    text: "Screen 2"
-                    on_release: root.screen_manager.current = "scr 2"
+class TestApp(MDApp):
 
-
-Screen:
-
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top": 1}
-        elevation: 10
-        title: "MDNavigationDrawer"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-
-    NavigationLayout:
-        x: toolbar.height
-
-        ScreenManager:
-            id: screen_manager
-
-            Screen:
-                name: "scr 1"
-
-                MDLabel:
-                    text: "Screen 1"
-                    halign: "center"
-
-            Screen:
-                name: "scr 2"
-
-                MDLabel:
-                    text: "Screen 2"
-                    halign: "center"
-
-        MDNavigationDrawer:
-            id: nav_drawer
-
-            ContentNavigationDrawer:
-                screen_manager: screen_manager
-                nav_drawer: nav_drawer
-'''
-
-
-class ContentNavigationDrawer(BoxLayout):
-    screen_manager = ObjectProperty()
-    nav_drawer = ObjectProperty()
-
-
-class TestNavigationDrawer(MDApp):
     def build(self):
-        return Builder.load_string(KV)
+        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.primary_hue = "300"
+        self.theme_cls.theme_style = "Light"
 
+        screen = Screen()
 
-TestNavigationDrawer().run()
+        self.button = Builder.load_string(button_helper)
+        self.username = Builder.load_string(username_helper)
+
+        screen.add_widget(self.username)
+        screen.add_widget(self.button)
+
+        return screen
+
+    def show_input(self):
+        print("Called")
+        self.dialog = MDDialog(text=f"Hello", title="Username", size_hint=(0.6,1))
+        self.dialog.open()
+        if self.username.text != "":
+            print(self.username.text)
+            self.username.text = ""
+
+TestApp().run()
